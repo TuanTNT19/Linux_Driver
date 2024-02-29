07d5fd780e92a38f9fbb6137ab96a1615316df8e
TuanTNT19 <tuantnt08@gmail.com>
2024-02-29 10:17:07 +0700

    update gpio device tree

---
+++ led_device_tree/gpio.c
@@ -0,0 +1,54 @@
+#include <linux/module.h>           /* Defines functions such as module_init/module_exit */
+#include <linux/gpio.h>             /* Defines functions such as gpio_request/gpio_free */
+#include <linux/platform_device.h>  /* For platform devices */
+#include <linux/gpio/consumer.h>    /* For GPIO Descriptor */
+#include <linux/of.h>               /* For DT */  
+
+#define DRIVER_AUTHOR "tuantnt08@gmail.com"
+#define DRIVER_DESC   "gpio test device tree"
+
+#define LOW     0
+#define HIGH    1
+
+struct gpio_desc *gpio2_3;
+
+static const struct of_device_id gpiod_dt_ids[] = {
+    { .compatible = "gpio_base_myself", },
+    { /* sentinel */ }
+};
+
+static int my_pdrv_probe(struct platform_device *pdev)
+{
+    struct device *dev = &pdev->dev;
+    gpio2_3 = gpiod_get(dev, "led67", GPIOD_OUT_LOW);
+    gpiod_set_value(gpio2_3, HIGH);
+
+    pr_info("%s - %d", __func__, __LINE__);
+    return 0;
+}
+
+static int my_pdrv_remove(struct platform_device *pdev)
+{
+    gpiod_set_value(gpio2_3, LOW);
+    gpiod_put(gpio2_3);
+
+    pr_info("%s - %d", __func__, __LINE__);
+    return 0;
+}
+
+/* platform driver */
+static struct platform_driver mypdrv = {
+    .probe = my_pdrv_probe,
+    .remove = my_pdrv_remove,
+    .driver = {
+        .name = "descriptor-based",
+        .of_match_table = of_match_ptr(gpiod_dt_ids),
+        .owner = THIS_MODULE,
+    },
+};
+module_platform_driver(mypdrv);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+MODULE_VERSION("1.0");
